eab2e9ff2dbeefbbcd94dcaf789a7573e63aacaf
Ubuntu.QA root <root@ubuntu.qa>
2014-11-17 17:05:53 +0000

    Make run-sync generic

--- run-sync.py
+++ run-sync.py
@@ -1,11 +1,17 @@
 from os.path import dirname, abspath, join
+import sys
+
 from bzr_sync import sync_git_to_bzr
 
 base_dir = dirname(abspath(__file__))
 
+if len(sys.argv) < 3:
+    print "Usage: run-sync.py {project_name} {git_user}"
+    exit(1)
+
 sync_git_to_bzr(
-    project_name='assets-manager',
-    git_user='canonicalltd',
+    project_name=sys.argv[1],
+    git_user=sys.argv[2],
     repositories_dir=join(base_dir, 'repositories')
 )
 
