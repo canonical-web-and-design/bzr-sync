bf39893b04707038d3ce8f5e79e1d2603d51fa5a
Robin Winslow <robin@robinwinslow.co.uk>
2014-11-26 11:31:51 +0000

    Hotfix: recipients

--- example.settings.py
+++ example.settings.py
@@ -1,3 +1,3 @@
 auth_token = 'ASECURETOKEN'  # You may want to use a UUID (`uuidgen`)
-error_email_recipient = 'strongbad@homestarrunner.com'
+error_email_recipients = 'strongbad@homestarrunner.com'
 error_email_sender = 'hal9000@one.discovery.theinfinitevoid'
